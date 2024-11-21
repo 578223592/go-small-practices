1d268deff4928acafa593c421cb3f551e3cae1ef
swx <cwl990110@163.com>
2024-11-22 00:49:34 +0800

    feat(test21): 添加 select 语句中关闭 channel 的处理逻辑
    - 新增 test2 函数，演示在 select 语句中处理 channel 关闭的情况
    - 创建一个 channel 并在协程中关闭它
    - 在 select 语句中接收 channel 数据，并打印结果
    - 最后打印 "ok" 表示 select 语句执行完毕

--- test21/main.go
+++ test21/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"time"
 )
 
 // 读channel的多返回值中的ok表示是这个数据是否有效，而不是此时的channel是否关闭
@@ -12,6 +13,24 @@ func test1() {
 	val, ok := <-oneChan
 	log.Printf("val:%d,ok:%v", val, ok) //2024/11/21 23:56:21 val:1,ok:true
 }
+
+// select过程中如果关闭chan
+func test2() {
+	oneChan := make(chan int)
+
+	go func() {
+		time.Sleep(time.Second)
+		close(oneChan)
+	}()
+
+	select {
+	case x, ok := <-oneChan:
+		log.Printf("x:%d,ok:%v", x, ok)
+	}
+	log.Printf("ok")
+
+}
 func main() {
-	test1()
+	// test1()
+	test2()
 }
